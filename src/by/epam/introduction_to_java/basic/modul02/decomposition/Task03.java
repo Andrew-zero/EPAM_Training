142a22294d98e43e635b2904f16326a715c6496f
Your Name <zerromon@gmail.com>
2021-12-03 00:55:34 +0300

    decomposotion_Task03

--- src/by/epam/introduction_to_java/basic/modul02/decomposition/Task03.java
+++ src/by/epam/introduction_to_java/basic/modul02/decomposition/Task03.java
@@ -6,4 +6,13 @@ package by.epam.introduction_to_java.basic.modul02.decomposition;
 Вычислить площадь правильного шестиугольника со стороной а, используя метод вычисления площади треугольника.
  */
 public class Task03 {
+
+    public static double hexagonSFromTriangle(double triangleS){
+        return triangleS * 6;
+    }
+
+    public static double triangleS(int a){
+        double p = (a *3)/2;
+        return Math.sqrt(p*(p-a)*3);
+    }
 }
