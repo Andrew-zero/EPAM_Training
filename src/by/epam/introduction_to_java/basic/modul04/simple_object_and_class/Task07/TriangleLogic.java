a9465f5ba5dcb22fa0b27e2c369415390b7c865b
Your Name <zerromon@gmail.com>
2021-12-27 08:38:25 +0300

    Triangle

--- src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task07/TriangleLogic.java
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task07/TriangleLogic.java
@@ -6,7 +6,18 @@ package by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task0
  */
 public class TriangleLogic {
 
-    public static double squareByTreeSide(Triangle triangle){
+    public static double squareByTreeSide(Triangle triangle) {
+        double p = (triangle.getA() + triangle.getB() + triangle.getC()) / 2;
 
+        return Math.sqrt(p * (p - triangle.getA()) * (p - triangle.getB()) * (p - triangle.getC()));
     }
+
+    private static double squareByFoundationAndHeight(Triangle triangle) {
+        return 1.5 * triangle.getA() * triangle.getHeight();
+    }
+
+    private static double squareBy2SideAndDegree(Triangle triangle) {
+        return 1.5 * triangle.getA() * triangle.getB() * triangle.getDegreeA();
+    }
+
 }
