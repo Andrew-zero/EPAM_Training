b29170767ee5e7e8cfd6610238e5a352add93ee5
Andrew <zerromon@gmail.com>
2021-12-28 14:20:38 +0300

    Customer

---
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task08/CustomerLogic.java
@@ -0,0 +1,13 @@
+package by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task08;
+
+
+
+/*
+Найти и вывести:
+а) список покупателей в алфавитном порядке;
+б) список покупателей, у которых номер кредитной карточки находится в заданном интервале.
+ */
+public class CustomerLogic {
+
+
+}
---
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task08/CustomerView.java
@@ -0,0 +1,12 @@
+package by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task08;
+
+
+
+/*
+Задать критерии выбора данных и вывести эти данные на консоль.
+ */
+public class CustomerView {
+
+
+
+}
