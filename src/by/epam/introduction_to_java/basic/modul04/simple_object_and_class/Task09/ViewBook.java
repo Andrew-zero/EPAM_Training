6fcc5e7ce8db3935428b9a73ae6d936be05cb9e9
Andrew <zerromon@gmail.com>
2022-01-03 11:04:45 +0300

    Book

--- src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task09/ViewBook.java
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task09/ViewBook.java
@@ -1,6 +1,10 @@
 package by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task09;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
 Задать критерии выбора данных и вывести эти данные на консоль.
  */
@@ -13,14 +17,25 @@ public class ViewBook {
         this.logicBook = logicBook;
     }
 
+    public int inputCriteria(){
+        int number = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            number = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return number;
+    }
+
     public void choseCriteria(){
         String message = "Выберите критерий выбора данных: ";
         print(message);
 
         for(Option o : Option.values()){
-            print(o.getNumber() + " " + o.getDescribe());
+            print(o.ordinal() + " " + o.getDescribe());
         }
-
     }
 
     public void printBook(Book book){
