3e7830d0b33b6c3024999d267dec2d3f9d96d205
Andrew <zerromon@gmail.com>
2022-01-03 11:32:02 +0300

    Book

--- src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task09/LogicBook.java
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task09/LogicBook.java
@@ -1,6 +1,9 @@
 package by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task09;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 Найти и вывести:
@@ -9,4 +12,51 @@ b)	список книг, выпущенных заданным издатель
 c)	список книг, выпущенных после заданного года.
  */
 public class LogicBook {
+    private BookShelf bookShelf;
+
+    public LogicBook() {
+    }
+
+    public LogicBook(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+    }
+
+    public List<Book> choseBookAfterInputYear(int year) {
+        ArrayList<Book> listBooks = new ArrayList<>();
+
+        for (Book book : getAllBook()) {
+            if (book.getYearOfPublication() > year) {
+                listBooks.add(book);
+            }
+        }
+
+        return listBooks;
+    }
+
+    public List<Book> chosePublishedHouse(String publishedHouse) {
+
+        return getAllBook().stream()
+                .filter(book -> book.getPublishedHouse().equalsIgnoreCase(publishedHouse))
+                .collect(Collectors.toList());
+    }
+
+    public List<Book> choseAuthor(String author) {
+
+        return getAllBook().stream()
+                .filter(book -> book.getAuthor().equalsIgnoreCase(author))
+                .collect(Collectors.toList());
+    }
+
+    public ArrayList<Book> getAllBook() {
+        return bookShelf.getBooks();
+    }
+
+    public BookShelf getBookShelf() {
+        return bookShelf;
+    }
+
+    public void setBookShelf(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+    }
+
 }
