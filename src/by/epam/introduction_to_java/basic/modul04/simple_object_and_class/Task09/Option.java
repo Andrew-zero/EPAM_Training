95da11699a265db9df2457351d99ce24c3c169c7
Andrew <zerromon@gmail.com>
2022-01-03 11:01:02 +0300

    Book

--- src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task09/Option.java
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task09/Option.java
@@ -1,20 +1,14 @@
 package by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task09;
 
 public enum Option {
-    ONE(1, "Cписок книг заданного автора"),
-    TWO(2, "Cписок книг, выпущенных заданным издательством"),
-    THREE(3, "Cписок книг, выпущенных после заданного года");
+    ONE("Cписок книг заданного автора"),
+    TWO("Cписок книг, выпущенных заданным издательством"),
+    THREE("Cписок книг, выпущенных после заданного года");
 
-    private int number;
     private String describe;
 
-    Option(int number, String describe){
-        this.number = number;
+    Option(String describe){
         this.describe = describe;
-    }
-
-    public int getNumber() {
-        return number;
     }
 
     public String getDescribe() {
