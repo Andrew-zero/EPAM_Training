fe29c457ea2420e026b72c2cd9461db03a3fa256
Andrew <zerromon@gmail.com>
2022-01-03 13:03:14 +0300

    Book

--- src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task09/LogicBook.java
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task09/LogicBook.java
@@ -21,7 +21,12 @@ public class LogicBook {
         this.bookShelf = bookShelf;
     }
 
-    public void initializationBook(){
+    public void initializationBook() {
+        ArrayList<Book> bookArrayList = getAllBook();
+
+        for(int i = 0; i < 20; i++){
+
+        }
 
 
     }
