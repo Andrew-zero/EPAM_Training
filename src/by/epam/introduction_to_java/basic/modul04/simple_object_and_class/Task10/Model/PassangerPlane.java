cdcec4f491ee883f71f551b160f8c83cf5825114
Andrew <zerromon@gmail.com>
2022-01-03 16:59:20 +0300

    AirLine

--- src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task10/Model/PassangerPlane.java
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task10/Model/PassangerPlane.java
@@ -1,11 +1,13 @@
 package by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task10.Model;
 
+import by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task10.ViewAirLine;
+
 public class PassangerPlane extends Plane {
 
     public PassangerPlane(){}
 
     @Override
     public void fly() {
-
+        ViewAirLine.print("Могу летать");
     }
 }
---
+++ src/by/epam/introduction_to_java/basic/modul04/simple_object_and_class/Task10/ViewAirLine.java
@@ -0,0 +1,10 @@
+package by.epam.introduction_to_java.basic.modul04.simple_object_and_class.Task10;
+
+public class ViewAirLine {
+
+
+
+    public static void print(String s){
+        System.out.println(s);
+    }
+}
