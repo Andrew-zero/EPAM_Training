1db10bee2d4b40c689da037161bc9cf61fd7f08b
Andrew <zerromon@gmail.com>
2022-01-10 13:55:55 +0300

    Task02

--- src/by/epam/introduction_to_java/basic/modul02/one_dimensional_array_sort/Main.java
+++ src/by/epam/introduction_to_java/basic/modul02/one_dimensional_array_sort/Main.java
@@ -9,7 +9,7 @@ public class Main {
 //        Task01.concatArrayStream(Task01.array, Task01.array2, 3);
 //        Task01.concatArrayTraditional(Task01.array, Task01.array2, 3);
 
-//        Task02.newChain(Task02.array, Task02.array2);
+        Task02.newChain(Task02.array2, Task02.array);
 //        Task03.sortByChoiceArray(Task03.array);
 //        Task04.sortingByExchanged(Task04.array);
 //        Task05.sortByInserts(Task05.array);
