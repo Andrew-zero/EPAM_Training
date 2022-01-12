a09ea5720e2a5a1cccd954634e30e8b55d82f076
Your Name <zerromon@gmail.com>
2022-01-12 09:37:33 +0300

    Tour

---
+++ src/by/epam/introduction_to_java/basic/modul04/agregation_and_composition/Task05/Excursion.java
@@ -0,0 +1,4 @@
+package by.epam.introduction_to_java.basic.modul04.agregation_and_composition.Task05;
+
+public class Excursion {
+}
--- src/by/epam/introduction_to_java/basic/modul04/agregation_and_composition/Task05/TouristicVoucher.java
+++ src/by/epam/introduction_to_java/basic/modul04/agregation_and_composition/Task05/TouristicVoucher.java
@@ -5,8 +5,14 @@ package by.epam.introduction_to_java.basic.modul04.agregation_and_composition.Ta
 /*
 5.	Туристические путевки. Сформировать набор предложений клиенту по выбору
 туристической путевки различного типа (отдых, экскурсии, лечение, шопинг, круиз и т. д.) для оптимального выбора.
+
 Учитывать возможность выбора транспорта, питания и числа дней.
 Реализовать выбор и сортировку путевок.
  */
-public class Task05 {
+public class TouristicVoucher {
+    private boolean vehicle;
+    private boolean nutrition;
+    private int countDay;
+
+
 }
