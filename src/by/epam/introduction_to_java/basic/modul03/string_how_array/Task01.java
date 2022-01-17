3121d66149115f371c6d33240f5732399cd4f0ef
Andrew <zerromon@gmail.com>
2022-01-17 16:33:37 +0300

    String

--- src/by/epam/introduction_to_java/basic/modul03/string_how_array/Task01.java
+++ src/by/epam/introduction_to_java/basic/modul03/string_how_array/Task01.java
@@ -1,6 +1,7 @@
 package by.epam.introduction_to_java.basic.modul03.string_how_array;
 
 
+import java.util.Locale;
 
 /*
 Работа со строкой как с массивом символов
@@ -8,4 +9,31 @@ package by.epam.introduction_to_java.basic.modul03.string_how_array;
 
  */
 public class Task01 {
+    public String[] strings = {"ADJWVDW", "VJNROE", "DJWOQDW", "DJPODHW", "DMJOLPHJW", "DJWWEFWFW"};
+
+    public static void main(String[] args) {
+        Task01 task01 = new Task01();
+
+        task01.castCamelToSnake_Case(task01.strings);
+
+        for (String s : task01.strings) {
+            System.out.println(s);
+        }
+    }
+
+    public String[] castCamelToSnake_Case(String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            char[] chars = strings[i].toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j] >= 65 && chars[j] <= 90) {   //используя нумерацию таблицы ASCII и
+                    chars[j] = (char) (chars[j] + 32);    //то что char можно представлять в виде int
+                }
+            }
+            strings[i] = new String(chars);
+        }
+
+        return strings;
+    }
+
+
 }
--- src/by/epam/introduction_to_java/basic/modul03/string_how_array/Task02.java
+++ src/by/epam/introduction_to_java/basic/modul03/string_how_array/Task02.java
@@ -7,4 +7,15 @@ package by.epam.introduction_to_java.basic.modul03.string_how_array;
 
  */
 public class Task02 {
+    public static String string = "fjwoenmf word mwpermwordm;l oermword wordjwljo, 1word word2 43word342j ()@$word()#$&";
+
+    public static void main(String[] args) {
+
+    }
+
+    public String replace(String str, String replaceble, String newSnippet){
+
+
+        return null;
+    }
 }
