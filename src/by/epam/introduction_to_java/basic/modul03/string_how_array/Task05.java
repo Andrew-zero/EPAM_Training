fff01e21a0ad17f18cb1ba92318d68388b3870b3
Your Name <zerromon@gmail.com>
2022-01-19 00:48:08 +0300

    Airline

--- src/by/epam/introduction_to_java/basic/modul03/string_how_array/Task05.java
+++ src/by/epam/introduction_to_java/basic/modul03/string_how_array/Task05.java
@@ -1,7 +1,7 @@
 package by.epam.introduction_to_java.basic.modul03.string_how_array;
 
 
-
+import java.util.Arrays;
 
 /*
 
@@ -9,4 +9,36 @@ package by.epam.introduction_to_java.basic.modul03.string_how_array;
 
  */
 public class Task05 {
+
+    public static String testString = "  fjwoenmf word mwpermwordm;l oermword  wordjwljo,    1word word2 43word342j ()@$word(& ";
+
+    public static void main(String[] args) {
+        System.out.println(new Task05().removeRedundantSpace(testString));
+    }
+
+    public char[] removeRedundantSpace(String s) {
+        char[] chars = s.toCharArray();
+        int firstIndex = 0;
+        int lastIndex = chars.length - 1;
+
+        while (true) {
+            if (chars[firstIndex] == ' ') {
+                firstIndex++;
+            } else {
+                break;
+            }
+        }
+        while (true) {
+            if (chars[lastIndex] == ' ') {
+                lastIndex--;
+            } else {
+                break;
+            }
+        }
+
+        char[] newArray = new char[chars.length - firstIndex + lastIndex];
+        System.arraycopy(chars, firstIndex, newArray, 0, chars.length - firstIndex + lastIndex);
+
+        return newArray;
+    }
 }
