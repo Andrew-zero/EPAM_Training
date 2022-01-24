b3c2305f38581f9def8826062ed9087ce18fe9a5
Andrew <zerromon@gmail.com>
2022-01-24 17:09:38 +0300

    String

--- src/by/epam/introduction_to_java/basic/modul03/work_with_regex/Task01/Parser.java
+++ src/by/epam/introduction_to_java/basic/modul03/work_with_regex/Task01/Parser.java
@@ -1,5 +1,33 @@
 package by.epam.introduction_to_java.basic.modul03.work_with_regex.Task01;
 
-public class Parser {
 
+import java.util.regex.Pattern;
+
+/*
+1.	Cоздать приложение, разбирающее текст (текст хранится в строке) и
+позволяющее выполнять с текстом три различных операции:
+- отсортировать абзацы по количеству предложений;
+- в каждом предложении отсортировать слова по длине;
+- отсортировать лексемы в предложении по убыванию количества вхождений заданного символа, а в случае равенства – по алфавиту.
+ */
+public class Parser {
+    private static Pattern pattern;
+
+    public static String[] parseParagraph(String s){
+        pattern = Pattern.compile("\n");
+
+        return pattern.split(s);
+    }
+
+    public static String[] parseSentence(String s){
+        pattern = Pattern.compile("[!?.]");
+
+        return pattern.split(s);
+    }
+
+    public static String[] parseWord(String s){
+        pattern = Pattern.compile("\\b");
+
+        return pattern.split(s);
+    }
 }
