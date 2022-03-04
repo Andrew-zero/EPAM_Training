16df85c32f6b8a7c73040a6eaa963846fc589231
Andrew <zerromon@gmail.com>
2022-03-04 15:12:07 +0300

    OOP

--- src/by/epam/introduction_to_java/basic/modul05/Task01/Main.java
+++ src/by/epam/introduction_to_java/basic/modul05/Task01/Main.java
@@ -1,6 +1,8 @@
 package by.epam.introduction_to_java.basic.modul05.Task01;
 
 
+import javax.naming.InvalidNameException;
+
 /*
 Задача 1.
 Создать объект класса Текстовый файл, используя классы Файл, Директория. Методы: создать, переименовать,
@@ -9,6 +11,20 @@ package by.epam.introduction_to_java.basic.modul05.Task01;
 public class Main {
 
     public static void main(String[] args) {
+        Directory directory = new Directory("C:\\somePath\\");
+        TextFile textFile;
+        ViewFile viewFile = new ViewFile();
+
+        try {
+            textFile = directory.createNewTextFile("NewTextFile");
+
+            textFile.addContent("АбраКадабра");
+
+            viewFile.print(textFile);
+        } catch (InvalidNameException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
