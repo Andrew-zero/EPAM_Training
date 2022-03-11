80c057cd67548d4d6ed7e4c3c1360e7706e0f943
Your Name <zerromon@gmail.com>
2022-03-11 18:17:30 +0300

    OOP

--- src/by/epam/introduction_to_java/basic/modul05/Task03/View.java
+++ src/by/epam/introduction_to_java/basic/modul05/Task03/View.java
@@ -3,23 +3,24 @@ package by.epam.introduction_to_java.basic.modul05.Task03;
 public class View {
     Calendar calendar;
 
-    public View(){}
+    public View() {
+    }
 
-    public View(Calendar calendar){
+    public View(Calendar calendar) {
         this.calendar = calendar;
     }
 
-    public void printAllDayOff(){
-        for(Calendar.DayOff dayOff : calendar.getDayOffList()){
+    public void printAllDayOff() {
+        for (Calendar.DayOff dayOff : calendar.getDayOffList()) {
             System.out.println(dayOff);
         }
     }
 
-    public void printDayOff(Calendar.DayOff dayOff){
+    public void printDayOff(Calendar.DayOff dayOff) {
         System.out.println(dayOff);
     }
 
-    public void printDate(){
+    public void printDate() {
         System.out.println(calendar.getTime());
     }
 }
