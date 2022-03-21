030bbecd1d59d3379a356b37b4674f6c3071c56c
Andrew <zerromon@gmail.com>
2022-03-21 12:21:20 +0300

    refactor

--- src/by/epam/introduction_to_java/basic/modul02/one_dimensional_array/Task01.java
+++ src/by/epam/introduction_to_java/basic/modul02/one_dimensional_array/Task01.java
@@ -8,15 +8,23 @@ import java.util.Random;
  */
 public class Task01 {
 
-    public static int taskOne(){
+    public static int taskOne() {
         int n = 1000;
         int k = 3;
 
         Random rand = new Random();
         int[] array = rand.ints(n, 0, 1000).toArray();
-        int sum = Arrays.stream(array)
-                .filter(s -> s%k == 0)
-                .sum();
+//        int sum = Arrays.stream(array)
+//                .filter(s -> s%k == 0)
+//                .sum();
+
+        int sum = 0;
+
+        for (int i : array) {
+            if (i % k == 0) {
+                sum += i;
+            }
+        }
 
         System.out.println(sum);
         return sum;
