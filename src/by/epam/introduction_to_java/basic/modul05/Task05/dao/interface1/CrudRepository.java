c857a8ac0c2430eb618b9eb3674ec3699de1c643
Your Name <zerromon@gmail.com>
2022-04-17 21:49:25 +0300

    OOP

--- src/by/epam/introduction_to_java/basic/modul05/Task05/dao/interface1/CrudRepository.java
+++ src/by/epam/introduction_to_java/basic/modul05/Task05/dao/interface1/CrudRepository.java
@@ -1,7 +1,15 @@
 package by.epam.introduction_to_java.basic.modul05.Task05.dao.interface1;
 
-import java.util.Optional;
+import java.util.List;
 
-public interface CrudRepository<T, ID> {
+public interface CrudRepository<T> {
+    void create(Enum type);
 
+    void update(T t, int price);
+
+    void delete(T t);
+
+    List<T> findAll();
+
+    void deleteAll();
 }
